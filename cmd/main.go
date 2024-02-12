83cda683059f2c9821fcebb2c450d97403b65143
chinnnawat <chin3896@gmail.com>
2024-02-12 10:02:45 +0700

    handler facts

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/github.com/chinnnawat/Practice_Golang_RestApi-Docker-Postgres/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Chinn!")
+		return c.SendString("Hello, Chinnawat!")
 	})
 
 	app.Listen(":3000")
